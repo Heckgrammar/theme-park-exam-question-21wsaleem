566c03bc63f8b8f36f792343749243766af98aaa
21wsaleem <144434650+21wsaleem@users.noreply.github.com>
2025-03-03 19:59:09 +0000

    Update Program.cs
    newest

--- BillCalculationTask/Program.cs
+++ BillCalculationTask/Program.cs
@@ -4,22 +4,17 @@
     {
         static void Main(string[] args)
         {
-            /* A theme park charges £15 per person for a daily ticket. If there are six or more people in a group, the group is given a £5 discount.
-               Write a C# program to calculate the total charge for a group of people visiting the theme park.
-               The program must:
-                • get the user to enter the number of people in a group
-                • calculate the total charge by:
-                    ○ charging £15 per person
-                    ○ reducing the total charge by £5 if there are six or more people
-                • output the total charge.
-            You should use meaningful variable name(s) inyour answer.
-            */
-            // Write your program and test it with the following test data
-            // 1: 4 people
-            // 2: 10 people
-            // 3: 6 people
-            // Commit your changes and add screnshot evidence of your program running to the Readme
-
+           			 Console.WriteLine(" please enter the number of people in the group "); 
+         int numberOfPeople = (Console.ReadLine()); 
+           Int price=15
+        totalCharge = numberOfPeople *price; 
+         Int reduction=5 
+        if (numberOfPeople >= 6) 
+        { 
+          totalCharge - reduction= totalCharge; 
+        } 
+        Console.WriteLine("The total charge is £" + totalCharge);
         }
     }
 }
+			
